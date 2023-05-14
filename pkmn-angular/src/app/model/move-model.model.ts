97224727012cbff797e14440df1fc126eea4bd8e
arcanlte <pinangayjoel@gmail.com>
2023-05-13 21:25:07 -0400

    Added models

---
+++ pkmn-angular/src/app/model/move-model.model.ts
@@ -0,0 +1,9 @@
+export interface MoveModel {
+  id: number,
+  name: string,
+  power: number,
+  pp: number,
+  type: string,
+  accuracy: number,
+  damageClass: string
+}
---
+++ pkmn-angular/src/app/model/pokemon-model.model.ts
@@ -0,0 +1,21 @@
+import { MoveModel } from "./move-model.model"
+
+export interface PokemonModel {
+  id: number,
+  name: string,
+  stats: Array<StatsModel>,
+  types: Type,
+  moves: Array<MoveModel>,
+  front_image: string,
+  back_image: string
+}
+
+export interface Type{
+  typeOne: string,
+  typeTwo?: string
+}
+
+export interface StatsModel{
+  base_stat: number,
+  name: string
+}
