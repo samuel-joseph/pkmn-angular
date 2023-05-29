2f019cd978f506d86ba7134c9bb3333fe0a67040
arcanlte <pinangayjoel@gmail.com>
2023-05-28 22:47:19 -0400

    refined

--- pkmn-angular/src/app/helper/pokemon.class.ts
+++ pkmn-angular/src/app/helper/pokemon.class.ts
@@ -1,10 +1,7 @@
 import { Injectable } from "@angular/core"
 import { PokemonService } from "../_services/pokemon/pokemon.service"
-import { PokemonModel, RegionPokemon } from "../model/pokemon-model.model"
-import { Observable, switchMap } from 'rxjs';
-import { getMove, getStats, getTypes
-} from "./pokemon-helper"
-import { regionPokemonsImage, region } from "./region-helper"
+import { RegionPokemon } from "../model/pokemon-model.model"
+import { regionPokemonsImage } from "./region-helper"
  
 
  @Injectable({ providedIn: 'root' })
@@ -30,6 +27,18 @@ import { regionPokemonsImage, region } from "./region-helper"
         firstPokemon = 252
         lastPokemon = 386
          break
+       case "sinnoh":
+         firstPokemon = 387
+         lastPokemon = 493
+         break
+       case 'unova':
+         firstPokemon = 494
+         lastPokemon = 649
+         break
+       case 'kalos':
+         firstPokemon = 650
+         lastPokemon = 719
+         break
      }
      for (let i = firstPokemon; i <= lastPokemon; i++){
        this.regionPokemons.push(regionPokemonsImage(`${i}`))
@@ -42,41 +51,5 @@ import { regionPokemonsImage, region } from "./region-helper"
      this.restApi.getPokemonMove(id).subscribe((res) => {
        this.pokemon = res
      })
-    // console.log(this.http.getPokemon(id))
-      // this.restApi.getPokemon(id).subscribe(data => {
-      //   let tempMove: any[] = []
-      //   let tempArr = []
-    
-      //   // move to a separate method
-      //   for (const moveIndex of data.moves) {
-      //     let moveDetails = moveIndex.version_group_details[0]
-      //     if (moveDetails.move_learn_method.name === "level-up" &&
-      //         moveDetails.level_learned_at>=15
-      //     )
-      //       this.restApi.getPokemonMove(`${moveIndex.move.url}`).subscribe(move => {
-      //         let tempObj = getMove(move)
-      //         if(tempObj.power){
-      //           tempMove.push(tempObj)
-      //         }
-      //       })
-      //   }
-
-      //   for (let i = 0; i < 6; i++){
-      //     let tempObj = getStats(data.stats[i])
-      //     tempArr.push(tempObj)
-      //   }
-      //   //move to a separate method
-      //   this.pokemon = {
-      //     id: data.id,
-      //     name: data.name,
-      //     stats: tempArr,
-      //     types: getTypes(data.types),
-      //     moves: tempMove,
-      //     front_image: data.sprites.front_default,
-      //     back_image: data.sprites.back_default
-      //   }
-      //   console.log("AM I HERE" ,this.pokemon)
-      //     return this.pokemon
-      // })
   }
 }
--- pkmn-angular/src/app/helper/region-helper.ts
+++ pkmn-angular/src/app/helper/region-helper.ts
@@ -1,18 +1,3 @@
-export const region = {
-  kanto: {
-    firstPokemon: 1,
-    lastPokemon: 151
-  },
-  johto: {
-    firstPokemon: 152,
-    lastPokemon: 251
-  },
-  hoenn: {
-    firstPokemon: 252,
-    lastPokemon: 386
-  }
-}
-
 export const regionPokemonsImage = (id: string) => {
   return {
     id: id,
