3de37c54385facac85fe0b165e0f9ab0b755bba2
arcanlte <pinangayjoel@gmail.com>
2023-06-11 09:32:20 -0400

    options functional

--- pkmn-angular/src/app/battle/battle/battle.component.ts
+++ pkmn-angular/src/app/battle/battle/battle.component.ts
@@ -10,8 +10,34 @@ export class BattleComponent implements OnInit{
   @Input() player1: PokemonModel[] = []
   @Input() player2: PokemonModel[] = []
 
+  currentPlayer1: PokemonModel[] = []
+  currentPlayer2: PokemonModel[] = []
+
+  playerOption: string
+
   ngOnInit(): void {
-    console.log(this.player1)
-    console.log(this.player2)
+    this.playerOption = 'default'
+    this.currentPlayer1 = this.player1.splice(0,1)
+    this.currentPlayer2 = this.player2.splice(0,1)
+  }
+
+  decisionOption(response: string) {
+    console.log("CHECKING")
+    this.playerOption = response
+  }
+
+  swapOption(pokemon: PokemonModel) {
+    console.log("CHECKING")
+    if (this.currentPlayer1) {
+      this.player1.push(...this.currentPlayer1)
+      
+      const index = this.player1.findIndex(val => val == pokemon)
+      
+      this.currentPlayer1.pop()
+      this.player1.splice(index, 1)
+      
+      this.currentPlayer1.push(pokemon)
+      this.playerOption = 'default'
+    }
   }
 }
