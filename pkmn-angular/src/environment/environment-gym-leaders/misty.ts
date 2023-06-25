ef829f7301a0fcd38cd1df2bf941071fabc7e7d2
arcanlte <pinangayjoel@gmail.com>
2023-06-25 01:36:54 -0400

    misty

--- pkmn-angular/src/environment/environment-gym-leaders/misty.ts
+++ pkmn-angular/src/environment/environment-gym-leaders/misty.ts
@@ -1,7 +1,9 @@
 import { GymLeader } from "src/app/model/gym-leader-model.model";
 
 export const misty: GymLeader = {
-  gymBadge: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/badges/1.png',
+  name: 'Misty',
+  gymBadge: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/badges/2.png',
+  gymImage: 'https://4.bp.blogspot.com/_eQ5BBnWj_T8/SV2-CHmkTMI/AAAAAAAAFuY/eSF40l5MGhk/s280/misty219-01.jpg',
   gymLose: false,
   gymPokemons: [
     {
