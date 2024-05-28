ae6a0ed250ffd2376d69109c904e799a0ecf8396
arcanlte <pinangayjoel@gmail.com>
2024-05-27 23:52:41 -0400

    essential features of backend is done

--- pkmn-expressjs/server.js
+++ pkmn-expressjs/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const authRoutes = require("./routes/auth.routes");
+const userRoutes = require("./routes/user.routes");
 const cors = require("cors");
 require("dotenv").config();
 
@@ -12,92 +14,17 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // Connect to MongoDB
-const dbURI =
-  "mongodb+srv://pinangayjoel:xZYAIG1NfoDPNkSU@cluster0.7siulj3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"; // Replace with your MongoDB connection string
-
 mongoose
-  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
 
-// Define a schema
-const itemSchema = new mongoose.Schema({
-  name: String,
-  quantity: Number,
-  price: Number,
-});
-
-const trainerSchema = new mongoose.Schema({
-  name: String,
-  avatar: String,
-  pokemons: Array,
-  badges: Number,
-});
-
-// Define a model
-const Item = mongoose.model("Item", itemSchema);
-const Trainer = mongoose.model("Trainer", trainerSchema);
-
-// CRUD operations
-
-// Get all items
-app.get("/api/items", (req, res) => {
-  Item.find()
-    .then((items) => res.json(items))
-    .catch((err) => res.status(500).json({ error: err.message }));
-});
-
-app.get("/api/trainer", (req, res) => {
-  Trainer.find()
-    .then((trainers) => res.json(trainers))
-    .catch((err) => res.status(500).json({ error: err.message }));
-});
-
-app.get("/api/trainer/:id", (req, res) => {
-  Trainer.findById(req.params.id)
-    .then((trainer) => res.json(trainer))
-    .catch((err) => res.status(500).json({ error: err.message }));
-});
-
-// Create a new item
-app.post("/api/items", (req, res) => {
-  const newItem = new Item({
-    name: req.body.name,
-    quantity: req.body.quantity,
-    price: req.body.price,
-  });
-  newItem
-    .save()
-    .then((item) => res.json(item))
-    .catch((err) => res.status(500).json({ error: err.message }));
-});
-
-app.post("/api/trainer", (req, res) => {
-  const newTrainer = new Trainer({
-    name: req.body.name,
-    avatar: req.body.avatar,
-    pokemons: req.body.pokemons,
-    badges: req.body.badges,
-  });
-  newTrainer
-    .save()
-    .then((item) => res.json(item))
-    .catch((err) => res.status(500).json({ error: err.message }));
-});
-
-// Update an item
-app.put("/api/items/:id", (req, res) => {
-  Item.findByIdAndUpdate(req.params.id, req.body, { new: true })
-    .then((item) => res.json(item))
-    .catch((err) => res.status(500).json({ error: err.message }));
-});
-
-// Delete an item
-app.delete("/api/items/:id", (req, res) => {
-  Item.findByIdAndDelete(req.params.id)
-    .then(() => res.json({ message: "Item deleted" }))
-    .catch((err) => res.status(500).json({ error: err.message }));
-});
+//Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
 
 // Start the server
 app.listen(PORT, () => {
